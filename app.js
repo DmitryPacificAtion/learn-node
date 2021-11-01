8acc0c464d162bcd9394d82ff4c47f5d70420854
Dmyto Bruslik <Dmytro_Bruslik@external.mckinsey.com>
2021-11-01 20:50:39 +0200

    Update main file

--- app.js
+++ app.js
@@ -1,49 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-  // console.log(req.url, req.method, req.headers);
-  const { url, method } = req;
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter message</title></head>');
-    res.write('<body>');
-    res.write('<h1>Hello kitty</h1>');
-    res.write('<form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form>');
-    res.write('</body>');
-    res.write('</html>');
-    return res.end(); // send request
-  }
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    });
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-
-      fs.writeFile('message.txt', message, (err) => {
-        if(err) {
-          throw Error(err);
-        }
-
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    });
-  }
-  res.write('<html>');
-  res.write('<head>');
-  res.write('<title>Hello me</title>');
-  res.write('</head>');
-  res.write('<body>');
-  res.write('<h1>Bla bla</h1>');
-  res.write('</body>');
-  res.write('</html>');
-  res.end(); // send request
-  // process.exit(); // to stop watcing mode
-});
+const server = http.createServer(routes.requestHendler);
 
 server.listen(3000);
