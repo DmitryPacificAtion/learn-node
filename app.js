ae043ffcdfe7e3b1173c56eb05badf432e4519cb
Dmitry Bruslik <8174450-bruslik.dmitry@users.noreply.gitlab.com>
2022-03-08 11:19:48 +0200

    LEarn express

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const http = require('http');
-const routes = require('./routes');
+const http = require("http");
+const express = require("express");
 
-const server = http.createServer(routes.requestHandler);
+const app = express();
 
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log()
+  next(); // Allows the request to continue to the next middleware in line
+});
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<h1>Catalog</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from express</h1>');
+});
+
+app.listen(3000);
