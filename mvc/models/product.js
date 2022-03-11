37a0e8e6826909d0cecba41cc91f6f88c670c032
Dmitry Bruslik <8174450-bruslik.dmitry@users.noreply.gitlab.com>
2022-03-11 21:09:28 +0200

    Refactoring

--- mvc/models/product.js
+++ mvc/models/product.js
@@ -3,17 +3,22 @@ const path = require("path");
 const rootDir = require("../util/path");
 const filePath = path.join(rootDir, "data", "products.json");
 
+const readProductsFromFile = (callback) => {
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      return callback([]);
+    }
+    callback(JSON.parse(file));
+  });
+};
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    fs.readFile(filePath, (error, file) => {
-      let products = [];
-      if (!error) {
-        products = JSON.parse(file);
-      }
+    readProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(filePath, JSON.stringify(products), (error) => {
         if (error) {
@@ -24,11 +29,6 @@ module.exports = class Product {
   }
 
   static getAll(callback) {
-    fs.readFile(filePath, (error, file) => {
-      if (error) {
-        return callback([]);
-      }
-      callback(JSON.parse(file));
-    });
+    readProductsFromFile(callback);
   }
 };
