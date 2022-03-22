0e074f871402d97335b251503734f3dd630def53
Dmitry Bruslik <8174450-bruslik.dmitry@users.noreply.gitlab.com>
2022-03-22 19:33:47 +0200

    Fix prettier

--- 02-express/index.js
+++ 02-express/index.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')))
-app.use("/admin", adminRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
