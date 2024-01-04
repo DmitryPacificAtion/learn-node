429cd9ee05f2f84d173928a695c1ccd297329616
DmitryPacificAtion <bruslik.dmitry@gmail.com>
2024-01-04 15:07:48 +0200

    Upload PDF files and convert it to txt

--- neebo-pdf-analysis/routes/upload.js
+++ neebo-pdf-analysis/routes/upload.js
@@ -2,98 +2,43 @@ const path = require('path');
 const fs = require('fs');
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const PDFParser = require('pdf-parse');
 const rootDir = require('../utils/path');
-const filePath = path.join(rootDir, 'uploadeded.pdf');
-const formidable = require('formidable');
+
+// Set storage for file uploads using multer
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router.get('/', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'upload-form.html'));
 });
 
-router.post('/analyze-pdf', async (req, res, next) => {
-  var form = new formidable.IncomingForm();
-  form.parse(req, function (err, fields, files) {
-    console.log('filepath', file.fileupload.filepath);
-    console.log('err', err);
-    res.write('File uploaded');
-    res.end();
-  });
-  return;
+router.post(
+  '/analyze-pdf',
+  upload.single('pdfFile'),
+  async (req, res, next) => {
+    try {
+      if (!req.file) {
+        return res.status(400).send('No file uploaded');
+      }
 
-  // fs.readFile(filePath, (error, file) => {
-  //   console.log('readFile', file);
-  //   if (!error) {
-  //     try {
-  //       pdfParser.loadPDF(file).then((data) => {
-  //         console.log('loadPDF', data);
-  //       }).catch((err) => console.log('Promise error', err));
-  //       // pdfParser.on('pdfParser_dataError', (errData) =>
-  //       //   console.error('pdfParser_dataError', errData)
-  //       // );
-  //       // pdfParser.on('pdfParser_dataReady', (pdfData) => {
-  //       //   console.log('pdfParser_dataReady', pdfData);
-  //       //   // fs.writeFile('./test.json', JSON.stringify(pdfData));
-  //       // });
-  //       // pdfParser.on('readable', (pdfData) => {
-  //       //   console.log('readable', pdfData);
-  //       //   // fs.writeFile('./test.json', JSON.stringify(pdfData));
-  //       // });
-  //       // pdfParser.on('data', (pdfData) => {
-  //       //   console.log('data', pdfData);
-  //       //   // fs.writeFile('./test.json', JSON.stringify(pdfData));
-  //       // });
-  //     } catch {
-  //       throw Error("Checkout existing uploaded.pdf. Delete it if it's empty");
-  //     }
-  //   }
+      const pdfBuffer = req.file.buffer;
 
-  // fs.writeFile(filePath, req.body.pdf, (error) => {
-  //   console.error('writeFile error', error);
-  // });
+      const data = await PDFParser(pdfBuffer);
+      const textContent = data.text;
 
-  //   fs.writeFile(filePath, req.body, (error) => {
-  //     console.error('writeFile error', error);
-  //   });
-  // });
+      // Save text content to a txt file
+      const fileName = `parsed_text_${Date.now()}.txt`;
+      fs.writeFileSync(fileName, textContent);
 
-  // pdfParser.on('pdfParser_dataError', (errData) =>
-  //   console.error('pdfParser_dataError', errData)
-  // );
-  // pdfParser.on('pdfParser_dataReady', (pdfData) => {
-  //   console.log('pdfParser_dataReady', pdfData);
-  //   // fs.writeFile('./test.json', JSON.stringify(pdfData));
-  // });
-  // pdfParser.on('readable', (pdfData) => {
-  //   console.log('readable', pdfData);
-  //   // fs.writeFile('./test.json', JSON.stringify(pdfData));
-  // });
-  // pdfParser.on('data', (pdfData) => {
-  //   console.log('data', pdfData);
-  //   // fs.writeFile('./test.json', JSON.stringify(pdfData));
-  // });
-});
+      res.send('File Uploaded!');
+      res.end();
+    } catch (error) {
+      console.error('Error:', error);
+      res.status(500).send('Error analyzing PDF');
+    }
+  }
+);
 
 module.exports = router;
-//Read the content of the pdf from the downloaded path
-//   var pdfParser = new PDFParser(this, 1);
-//   pdfParser.on("pdfParser_dataError", function (errData) {
-//      console.error(errData.parserError)
-//   });
-//   pdfParser.on("pdfParser_dataReady", function (pdfData) {
-//   //console.log('here is the content: '+pdfParser.getRawTextContent());
-//   this.assert.ok(pdfParser.getRawTextContent().indexOf(textToVerify) > -1);
-//   });
-
-//   pdfParser.loadPDF(filePath);
-// } else {
-//     console.log('OOPs file not present in the downloaded folder');
-//     //Throw an error if the file is not found in the path mentioned
-//     this.assert.ok(fs.existsSync(filePath));
-// }
-
-// fs.readFile(filePath, (error, file) => {
-//   if (error) {
-//     return callback([]);
-//   }
-//   callback(JSON.parse(file));
-// });
